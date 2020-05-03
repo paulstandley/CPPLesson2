275f8e9021fd39cb9ac9fda7d1e64c65dea3b05b
Paul Standley <paulstandley1972@gmail.com>
2020-05-03 17:02:07 +0100

    Global constants as internal variables

--- CPPLesson.h
+++ CPPLesson.h
@@ -1,6 +1,18 @@
 #ifndef CPPLESSON_H
 #define CPPLESSON_H
 
+// define your own namespace to hold constants
+namespace constants
+{
+    // constants have internal linkage by default
+    constexpr double pi{ 3.14159 };
+    constexpr double avogadro{ 6.0221413e23 };
+    constexpr double my_gravity{ 9.2 }; // m/s^2 -- gravity is light on this planet
+    // ... other related constants
+}
+
+void radius();
+
 void isOddEven();
 void post_pre_fix();
 
