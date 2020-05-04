a70ecb2beb8d1cebe80bc970d8a5a90b43a0f963
Paul Standley <paulstandley1972@gmail.com>
2020-05-04 18:49:44 +0100

    Static duration via static keyword

--- CPPLesson2.cpp
+++ CPPLesson2.cpp
@@ -7,12 +7,27 @@
 #include <cmath> // std::abs
 #include <algorithm> // std::max
 
+void incrementAndPrint()
+{
+    static int s_value{ 1 }; // static duration via static keyword.  This initializer is only executed once.
+    ++s_value;
+    std::cout << s_value << '\n';
+} // s_value is not destroyed here, but becomes inaccessible because it goes out of scope
 
+int generateID()
+{
+    static int s_itemID{ 0 };
+    return s_itemID++; // makes copy of s_itemID, increments the real s_itemID, then returns the value in the copy
+}
 
 int main()
 { 
 
     std::cout << "CPP" << std::endl;
+    incrementAndPrint();
+    incrementAndPrint();
+    incrementAndPrint();
+    std::cout << generateID() << std::endl;
 
     return 0;
 } 
